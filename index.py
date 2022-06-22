3e83a57df3a6eadc48b061cf727d46bc65e9e970
Hussein Nahlé <60662637+husseinnahle@users.noreply.github.com>
2022-06-22 11:33:18 -0400

    01 create db (#4)
    * add .db extension to .gitignore
    
    * add 'sujet' table to database
    
    * add .db extension to .gitignore
    
    * add .db extension to .gitignore
    
    * pycodestyle
    
    * us1
    
    * add newline
    
    * pycodestyle
    
    * pycodestyle
    
    * Update storyboard.md

--- index.py
+++ index.py
@@ -1,10 +1,15 @@
+import json
+import html
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask import g
-
+from flask import jsonify
 from .modules.database import Database
 
 app = Flask(__name__)
+app.config['JSON_SORT_KEYS'] = False
+DATA_FILE_PATH = 'static/data.json'
 
 
 def get_db():
@@ -19,6 +24,22 @@ def close_connection(exception):
     db = getattr(g, 'database', None)
     if db is not None:
         db.disconnect()
+
+
+# Initialiser la base de données
+@app.before_first_request
+def init_database():
+    db = get_db()
+    file = open(DATA_FILE_PATH)
+    data = json.load(file)
+    for i, key in enumerate(data):
+        db.insert_sujet(i, key, json.dumps(data[key]))
+    file.close()
+
+
+@app.errorhandler(404)
+def not_found_error(error):
+    return render_template('404.html', title="Erreur 404"), 404
 
 
 @app.route('/')
@@ -49,3 +70,47 @@ def a_propos():
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
+
+ 
+# Retourner un quiz
+@app.route('/api/quiz')
+def quiz():
+    nom_sujet = request.args.get('sujet')
+    nom_sous_sujet = request.args.get('sous-sujet')
+    numero_raw = request.args.get('numero', type=int)
+    try:
+        numero = int(numero_raw)  # ValueError
+        sujet = get_db().read_sujet_nom(nom_sujet)  # TypeError
+        quiz = sujet.get_quiz(nom_sous_sujet, numero)  # KeyError, IndexError
+    except ValueError:
+        # Retourner une erreur si le numero n'est pas un entier
+        err = "Le numero '" + html.escape(numero_raw) + "' n'existe pas."
+        return render_template("404.html", title="Erreur 404", err=err), 404
+    except TypeError:
+        # Retourner un statut 204 si aucun sujet n'a été trouvé
+        return ('', 204)
+    except KeyError:
+        # Retourner une erreur si le nom du sous-sujet n'existe pas
+        err = "Le sujet '" + html.escape(nom_sous_sujet) + "' n'existe pas."
+        return render_template("404.html", title="Erreur 404", err=err), 404
+    except IndexError:
+        # Retourner une erreur si le numero de la question du quiz n'existe pas
+        err = "Le numero '" + str(numero) + "' n'existe pas."
+        return render_template("404.html", title="Erreur 404", err=err), 404
+    return jsonify(quiz)
+
+
+# Retourner les sujets disponibles
+@app.route('/api/sujets')
+def sujets():
+    nom = request.args.get('nom')
+    if nom is None or len(nom) == 0:
+        # Retourner tous les sujets
+        sujets = get_db().read_all_sujet()
+        return jsonify({e.get_nom(): e.to_json() for e in sujets}), 200
+    # Retourner un sujet selon 'nom'
+    try:
+        sujet = get_db().read_sujet_nom(nom)
+    except TypeError:
+        return jsonify("Aucun sujet trouvé."), 204
+    return jsonify({sujet.get_nom(): sujet.to_json()}), 200
