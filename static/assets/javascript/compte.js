a3ebab59451198d108e655efa38613beb32c76e4
Jean-MichelLandry <jmlandry867@gmail.com>
2022-07-11 20:22:00 -0400

    ajout javascript compte

--- static/assets/javascript/compte.js
+++ static/assets/javascript/compte.js
@@ -2,16 +2,22 @@
 
 
 function edit(){
-    const paragraph = document.getElementById("edit");
+    const email = document.getElementById("email");
+    const name = document.getElementById("name");
+    const password = document.getElementById("password");
     const edit_button = document.getElementById("edit-button");
     
     if( edit_button.innerText == "Done") {
-        paragraph.contentEditable = false;
-        paragraph.style.backgroundColor = "#ffe44d";
+        email.contentEditable = false;
+        name.contentEditable = false;
+        password.contentEditable = false;
+        paragraph.style.backgroundColor =  rgb(145, 146, 146);;
         edit_button.innerText ="Edit"
     }else{
-        paragraph.contentEditable = true;
-        paragraph.style.backgroundColor = "#dddbdb";
+        email.contentEditable = true;
+        name.contentEditable = true;
+        password.contentEditable = true;
+        paragraph.style.backgroundColor = rgb(237, 248, 248);
         edit_button.innerText ="Done"
     }
 }
