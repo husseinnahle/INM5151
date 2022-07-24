ee1d7fcb786475687a482c7445e35e4781da43ff
ruben-romero <dh991903@ens.uqam.ca>
2022-07-23 20:57:45 -0400

    BD - Ajouté table 'user_type', maj table 'user'

--- db/database.sql
+++ db/database.sql
@@ -11,5 +11,15 @@ create table user (
   salt varchar(32),
   hash varchar(128),
   member integer,
-  progress text
+  progress text,
+  user_type_id integer references user_type (id)
 );
+
+create table user_type (
+  id integer primary key,
+  description varchar (20) unique
+);
+
+insert into user_type (id, description) values (1, 'admin');
+insert into user_type (id, description) values (2, 'instructor');
+insert into user_type (id, description) values (3, 'user');
