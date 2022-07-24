fc9bf0159d37f098d166016e49a9a8ef4d97e97b
kj191048 <=>
2022-07-24 11:11:54 -0400

    align

--- static/assets/javascript/scriptAdmin.js
+++ static/assets/javascript/scriptAdmin.js
@@ -1,20 +1,28 @@
 $(document).ready(function(){
     $(".add-row").click(function(){
         var Username = $("#Username").val();
-        var Email = $("#Emaill").val();
+        var Email = $("#Email").val();
         var Type = $("#Type").val();
         var Status = $("#Status").val();
-        var markup = "<tr><td><input type='checkbox' Username ='record'></td><td>" + Username + "</td><td>" 
-                                + Email + Type  + "</td><td>" + Status + "</td></tr>";
+        var markup = "<tr><td><input type='checkbox' name ='record'></td><td>" + Username + "</td><td>" 
+                                + Email +"</td><td>" + Type  + "</td><td>" + Status + "</td>" +
+                                "<td> <button type=\"button\" id=\"editBtn\">Edit</button></td></tr>" +
+                                "<td> <button type=\"button\" class=\"delete-row\" id=\"editBtn\">Delete</button></td>";
         $("table tbody").append(markup);
     });
     
     // Find and remove selected table rows
-    $(".delete-row").click(function(){
+    $(".delete-rows").click(function(){
         $("table tbody").find('input[name="record"]').each(function(){
             if($(this).is(":checked")){
                 $(this).parents("tr").remove();
             }
         });
+    });
+
+     // Find and remove selected table rows
+     $(".delete-row").click(function(){
+                $(this).parents("tr").remove();
+            
     });
 });
