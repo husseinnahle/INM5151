adcc2746e866a78ff006cf6fc09af4824383d0ad
kj191048 <=>
2022-07-24 10:51:45 -0400

    editAccount

--- static/assets/javascript/scriptAdmin.js
+++ static/assets/javascript/scriptAdmin.js
@@ -1,18 +1,20 @@
-$(document).ready(function () {
-    $('.editbtn').click(function () {
-        var currentTD = $(this).parents('tr').find('td');
-        if ($(this).html() == 'Edit') {                  
-            $.each(currentTD, function () {
-                $(this).prop('contenteditable', true)
-            });
-        } else {
-           $.each(currentTD, function () {
-                $(this).prop('contenteditable', false)
-            });
-        }
-
-        $(this).html($(this).html() == 'Edit' ? 'Save' : 'Edit')
-
+$(document).ready(function(){
+    $(".add-row").click(function(){
+        var Username = $("#Username").val();
+        var Email = $("#Emaill").val();
+        var Type = $("#Type").val();
+        var Status = $("#Status").val();
+        var markup = "<tr><td><input type='checkbox' Username ='record'></td><td>" + Username + "</td><td>" 
+                                + Email + Type  + "</td><td>" + Status + "</td></tr>";
+        $("table tbody").append(markup);
     });
-
+    
+    // Find and remove selected table rows
+    $(".delete-row").click(function(){
+        $("table tbody").find('input[name="record"]').each(function(){
+            if($(this).is(":checked")){
+                $(this).parents("tr").remove();
+            }
+        });
+    });
 });
